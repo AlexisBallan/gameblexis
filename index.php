624e077738983a8ca4c50c650795fd51bd09a279
Alexis <allexis.bal@hotmail.fr>
2023-02-08 17:12:00 -0500

    Suppression de la barre de recherche #7

--- index.php
+++ index.php
@@ -5,29 +5,10 @@
 <title>Accueil</title>
 <link rel="stylesheet" href="WebApp/_Layout/Accueil.css">
 
-<body class="body">
-  
-  <div class="zone-texte" style="padding-right: 36%; padding-left:37.5%;">
-    <input type="text" id="zone-texte" class="form-control" placeholder="Zone de recherhce" onkeyup="recherche()">
-  </div>
-
-  <div id="zoneResultat"></div>
-  
+<body class="body">  
   <?php
   include "donnee/JeuxDAO.php"; 
   $jeux = JeuxDAO::listerJeux();
-      // ENVOYER UN MAIL
-        /*
-        to      = 'julesbrulon@orange.fr';
-        $subject = 'test';
-        $message = 'salut';
-        $headers = 'From: gameblexis@hotmail.com' . "\r\n" .
-            'Reply-To: gameblexis@hotmail.com' . "\r\n" .
-            'X-Mailer: PHP/' . phpversion();
-        
-        mail($to, $subject, $message, $headers);
-
-        */
       $iteration = 0;
       foreach($jeux as $jeu)
       {
@@ -79,37 +60,6 @@
       }
       ?>
     </div>
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-
-    <script scr="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-
-    <script>
-      function recherche()
-      {
-        var lettres = document.getElementById("zone-texte").value;
-        //alert(lettres.length);
-        if(lettres.length == 0)
-        {
-          document.getElementById("zoneResultat").innerHTML = "";
-          return;
-        }
-        else
-        {
-          var request = new XMLHttpRequest();
-          request.onreadystatechange = function()
-          {
-            if(this.readyState == 4 && this.status == 200)
-            {
-              document.getElementById("zoneResultat").innerHTML = this.responseText;                            
-            }
-          };
-
-          request.open("GET", "recherche_jeux.php?game="+lettres, true);
-          request.send();
-        }
-      }
-    </script>
 </body>
 
 <?php include 'WebApp/_Layout/_Footer.php'?>
